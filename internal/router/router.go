86b19efdf0b4e475199dae54c0f1ce9f11954fbf
Ian Yong <ianyongyc@gmail.com>
2020-12-26 18:24:00 +0800

    Add base middleware

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,12 +2,28 @@ package router
 
 import (
 	"go-multitenancy/internal/api/handlers"
+	"time"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func New() *chi.Mux {
-	r := chi.NewRouter()
-	r.MethodFunc("GET", "/", handlers.HandleIndex)
-	return r
+	router := chi.NewRouter()
+	setUpMiddleware(router)
+	router.MethodFunc("GET", "/", handlers.HandleIndex)
+	return router
+}
+
+func setUpMiddleware(router *chi.Mux) {
+	// Injects a request ID in the context of each request
+	router.Use(middleware.RequestID)
+	// Sets a http.Request's RemoteAddr to that of either the X-Forwarded-For or X-Real-IP header
+	router.Use(middleware.RealIP)
+	// Logs requests
+	router.Use(middleware.Logger)
+	// Recovers from panics and return a 500 Internal Server Error
+	router.Use(middleware.Recoverer)
+	// Returns a 504 Gateway Timeout after 1 min
+	router.Use(middleware.Timeout(60 * time.Second))
 }
